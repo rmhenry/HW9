74ae45d370db3f0d901100c55e7c101a7c83be8f
rmhenry <reeselightning83@gmail.com>
2018-04-24 17:02:36 -0500

    Implemented Ex11_19 class methods to update the inventory array and create a new file. Tested with successful output.

--- HW9/Ex11_19.cs
+++ HW9/Ex11_19.cs
@@ -9,29 +9,57 @@ namespace HW9
 {
     public class Ex11_19
     {
-        //private string[,] updateFileContents;
-        //private string[,] inventoryFileContents;
-
         public void UpdateInventory(string inventoryFileName, string updateFileName)
         {
-            int lineCount = CountFileLines(updateFileName);
-            string[,] updateFileContents = new string[lineCount, 3];
+            string[,] updateFileArray = FileToArray(updateFileName);
+            string[,] inventoryFileArray = FileToArray(inventoryFileName);
+            //DisplayArrayContents(inventoryFileArray);
+            UpdateInventoryArray(inventoryFileArray, updateFileArray);
+            //DisplayArrayContents(inventoryFileArray);
 
-            lineCount = CountFileLines(inventoryFileName);
-            string[,] inventoryFileContents = new string[lineCount, 3];
-
-            FileToArray(updateFileName, updateFileContents);
-
-            for (int i = 0; i < updateFileContents.GetLength(0); i++)
-            {
-                Console.WriteLine($"Item Number {updateFileContents[i, 0]} | " +
-                    $"Item {updateFileContents[i, 1]} | Quantity {updateFileContents[i, 2]}");
-            }
+            string newFileName = NewFile(inventoryFileArray);
+            DisplayFileContents("Initial inventory file", inventoryFileName);
+            DisplayFileContents("Update to inventory file", updateFileName);
+            DisplayFileContents("Updated inventory file", newFileName);
 
             Console.ReadLine();
         }
 
-        private int CountFileLines(string fileName)
+        private static string[,] FileToArray(string fileName)
+        {
+            int lineCount = CountFileLines(fileName);
+            string[,] array = new string[lineCount, 3];
+            StreamReader fileReader = new StreamReader(fileName);
+            string line;
+            int i = 0;
+
+            while ((line = fileReader.ReadLine()) != null)
+            {
+                string[] currentLine = line.Split('|');
+                if (currentLine.Length == 3)
+                {
+                    array[i, 0] = currentLine[0].Trim();
+                    array[i, 1] = currentLine[1].Trim();
+                    array[i, 2] = currentLine[2].Trim();
+                }
+                else if (currentLine.Length == 2)
+                {
+                    array[i, 0] = currentLine[0].Trim();
+                    array[i, 1] = "N/A";
+                    array[i, 2] = currentLine[1].Trim();
+                }
+                else
+                {
+                    Console.WriteLine("The specified file does not have the expected number of values per line.");
+                }
+
+                i++;
+            }
+            fileReader.Close();
+            return array;
+        }
+
+        private static int CountFileLines(string fileName)
         {
             StreamReader fileReader = new StreamReader(fileName);
             int lineCount = 0;
@@ -45,39 +73,62 @@ namespace HW9
             return lineCount;
         }
 
-        private void FileToArray(string fileName, string[,] array)
+        private static void UpdateInventoryArray(string[,] inventoryArray, string[,] updateArray)
+        {
+            for (int i = 0; i < updateArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < inventoryArray.GetLength(0); j++)
+                {
+                    if (inventoryArray[j, 0] == updateArray[i, 0])
+                    {
+                        int originalQuantity = int.Parse(inventoryArray[j, 2]);
+                        int quantityChange = int.Parse(updateArray[i, 2]);
+                        string newQuantity = (originalQuantity + quantityChange).ToString();
+                        inventoryArray[j, 2] = newQuantity;
+                    }
+                }
+            }
+        }
+
+        private static string NewFile(string[,] array)
+        {
+            string fileName = "newInventory.txt";
+            StreamWriter s = new StreamWriter(fileName);
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                s.WriteLine($"{array[i, 0]} | " +
+                    $"{array[i, 1]} | {array[i, 2]}");
+            }
+
+            s.Close();
+            return fileName;
+        }
+
+        private static void DisplayFileContents(string heading, string fileName)
         {
             StreamReader fileReader = new StreamReader(fileName);
             string line;
 
+            Console.WriteLine(heading);
+
             while ((line = fileReader.ReadLine()) != null)
             {
-                for (int i = 0; i < array.GetLength(0); i++)
-                {
-                    string[] currentLine = line.Split('|');
-                    if (currentLine.Length == 3)
-                    {
-                        array[i, 0] = currentLine[0].Trim();
-                        array[i, 1] = currentLine[1].Trim();
-                        array[i, 2] = currentLine[2].Trim();
-                    }
-                    else if (currentLine.Length == 2)
-                    {
-                        array[i, 0] = currentLine[0].Trim();
-                        array[i, 1] = "N/A";
-                        array[i, 2] = currentLine[1].Trim();
-                    }
-                    else
-                    {
-                        Console.WriteLine("The specified file does not have the expected number of values per line.");
-                    }
-
-
-                }
-
+                Console.WriteLine(line);
             }
 
             fileReader.Close();
+            Console.WriteLine();
+        }
+
+        private static void DisplayArrayContents(string[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.WriteLine($"{array[i, 0]} | " +
+                    $"{array[i, 1]} | {array[i, 2]}");
+            }
+
         }
     }
 }
